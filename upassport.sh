ab503cce7ce64e513aa099db42656d6e880d7cae
fred <support@qo-op.com>
2025-02-03 18:21:47 +0100

    NOSTR CARD : Using ${HOME}/.zen/Astroport.ONE/tools/make_NOSTRCARD.sh "${EMAIL}" "$IMAGE"

--- upassport.sh
+++ upassport.sh
@@ -153,7 +153,8 @@ if [[ ${QRCODE:0:4} == "http" ]]; then
     fi
 fi
 
-# CHECK IF IT IS AN EMAIL (SIMPLE REGEX CHECK)
+####################################################################
+# CHECK IF IT IS AN EMAIL = NOSTR CARD
 if [[ $QRCODE =~ ^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$ ]]; then
     EMAIL="$QRCODE"
     echo "Email detected: $EMAIL"
@@ -169,116 +170,8 @@ if [[ $QRCODE =~ ^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$ ]]; then
         exit 0
     fi
 
-    ############################################## PREPARE SALT PEPPER
-    SALT=$(tr -dc 'a-zA-Z0-9' < /dev/urandom | fold -w42 | head -n1)
-    PEPPER=$(tr -dc 'a-zA-Z0-9' < /dev/urandom | fold -w42 | head -n1)
-    # Creating a NOSTRCARD for ${EMAIL}
-    DISCO="/?${EMAIL}=${SALT}&nostr=${PEPPER}"
-    echo "DISCO : "$DISCO
-
-    ## ssss-split : Keep 2 needed over 3
-    echo "$DISCO" | ssss-split -t 2 -n 3 -q > ${MY_PATH}/tmp/${EMAIL}.ssss
-    HEAD=$(cat ${MY_PATH}/tmp/${EMAIL}.ssss | head -n 1) && echo "$HEAD" > ${MY_PATH}/tmp/${EMAIL}.ssss.head
-    MIDDLE=$(cat ${MY_PATH}/tmp/${EMAIL}.ssss | head -n 2 | tail -n 1) && echo "$MIDDLE" > ${MY_PATH}/tmp/${EMAIL}.ssss.mid
-    TAIL=$(cat ${MY_PATH}/tmp/${EMAIL}.ssss | tail -n 1) && echo "$TAIL" > ${MY_PATH}/tmp/${EMAIL}.ssss.tail
-    echo "TEST DECODING..."
-    echo "$HEAD
-    $TAIL" | ssss-combine -t 2 -q
-    [ ! $? -eq 0 ] && echo "ERROR! SSSSKEY DECODING FAILED" && echo "${MY_PATH}/templates/wallet.html" && exit 1
-
-    # 1. Generate a DISCO Nostr key pair
-    NPRIV=$(${MY_PATH}/tools/keygen -t nostr "${SALT}" "${PEPPER}" -s)
-    NPUBLIC=$(${MY_PATH}/tools/keygen -t nostr "${SALT}" "${PEPPER}")
-    echo "Nostr Private Key: $NPRIV"
-    echo "Nostr Public Key: $NPUBLIC"
-
-    # 2. Store the keys in a file or a secure place (avoid printing them to console if possible)
-    echo "$NPRIV" > ${MY_PATH}/tmp/${EMAIL}.nostr.priv
-    echo "$NPUBLIC" > ${MY_PATH}/tmp/${EMAIL}.nostr.pub
-
-    # Create an G1CARD : G1Wallet waiting for G1 to make key batch running
-    ${MY_PATH}/tools/keygen -t duniter -o ${MY_PATH}/tmp/${EMAIL}.g1card.dunikey "${SALT}" "${PEPPER}"
-    G1PUBNOSTR=$(cat ${MY_PATH}/tmp/${EMAIL}.g1card.dunikey  | grep 'pub:' | cut -d ' ' -f 2)
-    echo "G1NOSTR _WALLET: $G1PUBNOSTR"
-    mkdir -p ${HOME}/.zen/game/nostr/${EMAIL}/
-    [[ -s ${IMAGE} ]] && cp ${IMAGE} ${HOME}/.zen/game/nostr/${EMAIL}/picture.png
-
-    ##########################################################################
-    ### CRYPTO ZONE
-    ## ENCODE HEAD SSSS SECRET WITH G1PUBNOSTR PUBKEY
-    echo "${MY_PATH}/tools/natools.py encrypt -p $G1PUBNOSTR -i ${MY_PATH}/tmp/${EMAIL}.ssss.head -o ${HOME}/.zen/game/nostr/${EMAIL}/ssss.nostr.enc"
-    ${MY_PATH}/tools/natools.py encrypt -p $G1PUBNOSTR -i ${MY_PATH}/tmp/${EMAIL}.ssss.head -o ${HOME}/.zen/game/nostr/${EMAIL}/ssss.head.nostr.enc
-
-    ## DISCO MIDDLE ENCRYPT WITH CAPTAING1PUB
-    echo "${MY_PATH}/tools/natools.py encrypt -p $CAPTAING1PUB -i ${MY_PATH}/tmp/${EMAIL}.ssss.mid -o ${HOME}/.zen/game/nostr/${EMAIL}/ssss.mid.captain.enc"
-    ${MY_PATH}/tools/natools.py encrypt -p $CAPTAING1PUB -i ${MY_PATH}/tmp/${EMAIL}.ssss.mid -o ${HOME}/.zen/game/nostr/${EMAIL}/ssss.mid.captain.enc
-
-    ## DISCO TAIL ENCRYPT WITH UPLANETNAME
-    cat ${MY_PATH}/tmp/${EMAIL}.ssss.tail | gpg --symmetric --armor --batch --passphrase "${UPLANETNAME}" -o ${HOME}/.zen/game/nostr/${EMAIL}/ssss.tail.uplanet.asc
-    cat ${HOME}/.zen/game/nostr/${EMAIL}/ssss.tail.uplanet.asc | gpg -d --passphrase "${UPLANETNAME}" --batch > ${MY_PATH}/tmp/${G1PUBNOSTR}.ssss.test
-    [[ $(diff -q ${MY_PATH}/tmp/${G1PUBNOSTR}.ssss.test ${MY_PATH}/tmp/${EMAIL}.ssss.tail) != "" ]] && echo "ERROR: GPG ENCRYPTION FAILED !!!"
-    rm ${MY_PATH}/tmp/${G1PUBNOSTR}.ssss.test
-
-    ## CREATE IPNS KEY (SIDE STORAGE)
-    ${MY_PATH}/tools/keygen -t ipfs -o ${MY_PATH}/tmp/${MOATS}.nostr.ipns "${SALT}" "${PEPPER}"
-    ipfs key rm "${G1PUBNOSTR}:NOSTR" > /dev/null 2>&1
-    NOSTRNS=$(ipfs key import "${G1PUBNOSTR}:NOSTR" -f pem-pkcs8-cleartext ${MY_PATH}/tmp/${MOATS}.nostr.ipns)
-    echo "${G1PUBNOSTR}:NOSTR ${EMAIL} STORAGE: /ipns/$NOSTRNS"
-    echo "/ipns/$NOSTRNS" > ${HOME}/.zen/game/nostr/${EMAIL}/NOSTRNS
-
-    amzqr "${myIPFS}/ipns/$NOSTRNS" -l H -p ${MY_PATH}/static/img/no_str.png -c -n ${G1PUBNOSTR}.IPNS.QR.png -d ${MY_PATH}/tmp/ 2>/dev/null
-    convert ${MY_PATH}/tmp/${G1PUBNOSTR}.IPNS.QR.png \
-        -gravity SouthWest \
-        -pointsize 18 \
-        -fill black \
-        -annotate +2+2 "[APP] $NOSTRNS" \
-        -annotate +1+3 "[APP] $NOSTRNS" \
-        ${HOME}/.zen/game/nostr/${EMAIL}/IPNS.QR.png
-
-    VAULTNSQR=$(ipfs add -q ${HOME}/.zen/game/nostr/${EMAIL}/IPNS.QR.png)
-    ipfs pin rm /ipfs/${VAULTNSQR}
-
-    ## HEAD SSSS CLEAR
-    amzqr "$(cat ${MY_PATH}/tmp/${EMAIL}.ssss.head)" -l H -p ${MY_PATH}/static/img/key.png -c -n ${EMAIL}.QR.png -d ${MY_PATH}/tmp/ 2>/dev/null
-    SSSSQR=$(ipfs add -q ${MY_PATH}/tmp/${EMAIL}.QR.png)
-    ipfs pin rm /ipfs/${SSSSQR}
-
-    ## Create G1PUBNOSTR QR Code
-    [[ -s ${HOME}/.zen/game/nostr/${EMAIL}/picture.png ]] \
-        && DPIC="${HOME}/.zen/game/nostr/${EMAIL}/picture.png" \
-        || DPIC="${MY_PATH}/static/img/nature_cloud_face.png"
-    amzqr "${G1PUBNOSTR}" -l H -p ${DPIC} -c -n G1PUBNOSTR.QR.png -d ${MY_PATH}/tmp/ 2>/dev/null
-    echo "${G1PUBNOSTR}" > ${HOME}/.zen/game/nostr/${EMAIL}/G1PUBNOSTR
-    convert ${MY_PATH}/tmp/G1PUBNOSTR.QR.png \
-            -gravity SouthWest \
-            -pointsize 18 \
-            -fill black \
-            -annotate +2+2 "${G1PUBNOSTR}" \
-            -annotate +1+3 "${G1PUBNOSTR}" \
-            ${HOME}/.zen/game/nostr/${EMAIL}/G1PUBNOSTR.QR.png
-
-    G1PUBNOSTRQR=$(ipfs add -q ${HOME}/.zen/game/nostr/${EMAIL}/G1PUBNOSTR.QR.png)
-    ipfs pin rm /ipfs/${G1PUBNOSTRQR}
-
-    ##############################################################
-    ### PREPARE NOSTR ZINE
-    cat ${MY_PATH}/static/zine/nostr.html \
-    | sed -e "s~npub1w25fyk90kknw499ku6q9j77sfx3888eyfr20kq2rj7f5gnm8qrfqd6uqu8~${NPUBLIC}~g" \
-            -e "s~nsec13x0643lc3al5fk92auurh7ww0993syj566eh7ta8r2jpkprs44rs33cute~${NPRIV}~g" \
-            -e "s~toto@yopmail.com~${EMAIL}~g" \
-            -e "s~QmdmeZhD8ncBFptmD5VSJoszmu41edtT265Xq3HVh8PhZP~${SSSSQR}~g" \
-            -e "s~Qma4ceUiYD2bAydL174qCSrsnQRoDC3p5WgRGKo9tEgRqH~${G1PUBNOSTRQR}~g" \
-            -e "s~Qmeu1LHnTTHNB9vex5oUwu3VVbc7uQZxMb8bYXuX56YAx2~${VAULTNSQR}~g" \
-            -e "s~_NOSTRVAULT_~/ipns/${NOSTRNS}~g" \
-            -e "s~_CAPTAINEMAIL_~${CAPTAINEMAIL}~g" \
-            -e "s~_NOSTRG1PUB_~${G1PUBNOSTR}~g" \
-            -e "s~_UPLANET8_~UPlanet:${UPLANETG1PUB:0:8}~g" \
-            -e "s~_DATE_~$(date -u)~g" \
-            -e "s~http://127.0.0.1:8080~${myIPFS}~g" \
-        > ${HOME}/.zen/game/nostr/${EMAIL}/_index.html
-
-    NOSTRIPFS=$(ipfs add -rwq ${HOME}/.zen/game/nostr/${EMAIL}/ | tail -n 1)
-    ipfs name publish --key "${G1PUBNOSTR}:NOSTR" /ipfs/${NOSTRIPFS} 2>&1 >/dev/null &
+    ### CREATING NOSTR CARD
+    ${HOME}/.zen/Astroport.ONE/tools/make_NOSTRCARD.sh "${EMAIL}" "$IMAGE"
 
     ## MAILJET SEND NOSTR CARD
     ${HOME}/.zen/Astroport.ONE/tools/mailjet.sh "${EMAIL}" "${HOME}/.zen/game/nostr/${EMAIL}/_index.html" "NOSTR Card $NPUBLIC"
