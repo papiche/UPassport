8690f170959b4829bcd8a89345a952daaf2fdeea
fred <support@qo-op.com>
2025-04-29 04:03:05 +0200

    nnn

--- upassport.sh
+++ upassport.sh
@@ -254,7 +254,7 @@ if [[ ${PUBKEY:0:2} == "1-" && ${ZCHK:0:6} == "k51qzi" ]]; then
 
         # Combine "$tmp_player" "$tmp_tail"  decrypted shares ## STRANGE 1, 2 OUTPUT
         # WARNING: couldn't get memory lock (ENOMEM, try to adjust RLIMIT_MEMLOCK!).*
-        DISCO=$(cat "$tmp_player" "$tmp_tail" | ssss-combine -t 2 -q | tail -f 1)
+        DISCO=$(cat "$tmp_player" "$tmp_tail" | ssss-combine -t 2 -q | tail -n 1)
         echo "DISCO = $DISCO"
         IFS='=&' read -r s salt p pepper <<< "$DISCO"
         echo "s=$s"
